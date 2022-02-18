da8678c819b110aad59dffae3b25d10ba2788e9a
AuroreTr <aurore.trinci@gmail.com>
2022-02-18 18:55:41 +0100

    avancement premier algo avec table de hash

--- test2/js/services/datamanager.js
+++ test2/js/services/datamanager.js
@@ -1,35 +1,52 @@
 import recipes from "../../../recipes.js";
 import Machin from "./machin.js";
 
-const ingredients  = new Machin(recipes, "ingredients");
-const titles  = new Machin(recipes, "name");
-const appliance  = new Machin(recipes, "appliance");
-const ustensils  = new Machin(recipes, "ustensils");
-
-console.log(ingredients)
-console.log(titles);
+const ingredients = new Machin(recipes, "ingredients");
+const titles = new Machin(recipes, "name");
+const appliance = new Machin(recipes, "appliance");
+const ustensils = new Machin(recipes, "ustensils");
 
 /**
- * [getRecipes description]
+ * get the final recipes according to the data entries of the search bar and the selected tags
  *
- * @return  {Array}  [return the array of the recipes par rapport aux entrée de la barre de recherche]
+ * @param   {String}  input  the data entries of the search bar
+ *
+ * @return  {Array}         the array of the index of final recipes
  */
-function getRecipes(){
-    let validRecipesId = commonEntries( ingredients.validRecipesId, titles.validRecipesId);
-    validRecipesId = commonEntries (validRecipesId, appliance.validRecipesId);
-    //TODO compléter
-    const finalRecipes = [];
-    validRecipesId.forEach(recipeId => {
-        finalRecipes.push(recipes[recipeId])
+function getRecipes(input) {
+  const validRecipesIdByIngrendients = ingredients.hashTable[input];
+  const validRecipesIdByTitles = titles.hashTable[input];
+  const validRecipesIdByAppliance = appliance.hashTable[input];
+  const validRecipesIdByUstensils = ustensils.hashTable[input];
+
+  let validRecipesId = commonEntries(validRecipesIdByIngrendients, validRecipesIdByTitles);
+  validRecipesId = commonEntries(validRecipesId, validRecipesIdByAppliance);
+  validRecipesId = commonEntries(validRecipesId, validRecipesIdByUstensils);
+
+  const finalRecipes = [];
+  validRecipesId.forEach((recipeId) => {
+    finalRecipes.push(recipes[recipeId]);
+  });
+  return finalRecipes;
+}
+
+/**
+ * compare the values of two arrays to get the common value(s)
+ *
+ * @param   {Array}  arrayA  the first array to compare
+ * @param   {Array}  arrayB  the second array to compare
+ *
+ * @return  {Array}          the array of the common values
+ */
+function commonEntries(arrayA, arrayB) {
+  let commonRecipesId = [];
+  for (let i = 0; i < arrayA.length; i++) {
+    arrayB.forEach(element => {
+      arrayA[i] === element ? commonRecipesId.push(element) : false;
     });
-    return finalRecipes;
+  }
+  return commonRecipesId;
 }
+  
 
-function commonEntries(arrayA, arrayB){
-    return []; //TODO finir la fonction
-}
-
-
-export {
-    getRecipes
-}
+export { getRecipes };
---
+++ test2/js/services/exHash.js
@@ -0,0 +1,12 @@
+const hashs = {
+    "fra" : [12, 17, 99],
+    "frai" : [12, 99],
+    "frais" : [12, 99],
+    "fraise" : [12, 99],
+    "fram" : [17],
+    "framb" : [17]
+}
+
+const input= "frai";
+
+const ids = hashs[input]
